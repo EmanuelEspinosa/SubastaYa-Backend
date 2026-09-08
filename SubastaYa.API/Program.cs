50cb4730fa6ae4c8bdbddf2f392d8783e4d261b4
Emanuel Espinosa <emanuel.espinosa.roberto@gmail.com>
2026-09-08 04:01:30 -0300

    config: automatizar migraciones de BD y habilitar Swagger al iniciar la API

--- SubastaYa.API/Program.cs
+++ SubastaYa.API/Program.cs
@@ -14,7 +14,14 @@ builder.Services.AddDbContext<SubastaYaDbContext>(options =>
 
 var app = builder.Build();
 
-// Configurar el pipeline HTTP
+// 1. Aplicar migraciones en la base de datos al arrancar
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<SubastaYaDbContext>();
+    dbContext.Database.Migrate();
+}
+
+// 2. Configurar el pipeline HTTP
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
