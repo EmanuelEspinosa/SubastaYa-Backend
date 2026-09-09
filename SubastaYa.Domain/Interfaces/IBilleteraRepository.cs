dccbed7cab1998f1e19c2be8f8e713b247b4013e
Emanuel Espinosa <emanuel.espinosa.roberto@gmail.com>
2026-09-08 23:43:19 -0300

    feat(dominio): agregar interfaces de repositorios para Subasta y Billetera
    Se definen los contratos ISubastaRepository e IBilleteraRepository en la capa Domain para cumplir con el Principio de Inversión de Dependencias (DIP) y preparar el manejo de transacciones.

---
+++ SubastaYa.Domain/Interfaces/IBilleteraRepository.cs
@@ -0,0 +1,17 @@
+﻿using SubastaYa.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubastaYa.Domain.Interfaces
+{
+    internal interface IBilleteraRepository
+    {
+        Task<Billetera?> ObtenerPorUsuarioIdAsync(int usuarioId);
+
+        // Fundamental para el bloque transaccional atómico
+        Task ActualizarAsync(Billetera billetera);
+    }
+}
---
+++ SubastaYa.Domain/Interfaces/ISubastaRepository.cs
@@ -0,0 +1,24 @@
+﻿using SubastaYa.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubastaYa.Domain.Interfaces
+{
+    internal interface ISubastaRepository
+    {
+        // Para Catálogo y Exploración
+        Task<IEnumerable<Subasta>> ObtenerTodasAsync();
+
+        // Para Detalle y validación antes de pujar
+        Task<Subasta?> ObtenerPorIdAsync(int id);
+
+        // Para publicación de nueva subasta por el Vendedor
+        Task AgregarAsync(Subasta subasta);
+
+        // Para aplicar la regla Anti-Sniping y actualizar el estado
+        Task ActualizarAsync(Subasta subasta);
+    }
+}
